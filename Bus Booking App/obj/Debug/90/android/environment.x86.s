adab55bb492c1e296844cae762343cbd1a6e8224
MrMalith <sachinthamalith047@gmail.com>
2021-01-08 23:20:25 +0530

    update env

--- Bus Booking App/obj/Debug/90/android/environment.x86.s
+++ Bus Booking App/obj/Debug/90/android/environment.x86.s
@@ -61,7 +61,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"a6da5165-74a9-44f0-84db-93dae2a2c99c"
+	.asciz	"abbfa9ac-bbdc-4026-9f93-a203909bf85b"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
